b1fc2079a01037e0d948a8ba85bb088440205fe7
Gasol Wu <gasol.wu@gmail.com>
2014-05-08 00:09:07 +0800

    create object should return object with default domain

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 var assert = require('assert');
+var nis = require('../build/Release/nis');
+assert.equal('object', typeof(nis));
 
 describe('NIS', function() {
-    describe('require', function() {
-        it('should return object', function() {
-            var nis = require('../build/Release/nis');
-            assert.equal('object', typeof(nis));
+    describe('create object', function() {
+        it('should return object with default domain', function() {
+            var yp = nis();
+            assert.equal('object', typeof(yp));
         })
     })
 });
