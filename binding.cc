f5bb875f4f18aa5b233380a526d53957cb1d3b49
Gasol Wu <gasol.wu@gmail.com>
2014-05-08 00:15:27 +0800

    new object

--- binding.cc
+++ binding.cc
@@ -3,7 +3,16 @@
 
 using namespace v8;
 
-void init(Handle<Object> exports) {
+Handle<Value> CreateObject(const Arguments& args) {
+    HandleScope scope;
+
+    Local<Object> obj = Object::New();
+    return scope.Close(obj);
+}
+
+void init(Handle<Object> exports, Handle<Object> module) {
+    module->Set(String::NewSymbol("exports"),
+            FunctionTemplate::New(CreateObject)->GetFunction());
 }
 
 NODE_MODULE(nis, init);
